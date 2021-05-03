4729ccc479e535f30cb5d278a0cf5bd7c59eb153
aesteves-inw <58950840+aesteves-inw@users.noreply.github.com>
2021-05-03 15:45:42 +0100

    updates 03052021

---
+++ src/test/java/d03/SOI_3593/SOI_3593_TC56_DS_D03_OfferGeneration_Temporary_ISDN.java
@@ -0,0 +1,72 @@
+package d03.SOI_3593;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import delivery03.SOI_3593;
+import executionDriver.ExecDriverClass;
+import executionTools.ExecStructure;
+import executionTools.TestStructure;
+import testLogBuilder.TestLog;
+import testLogger.TestLogger;
+import testReportComposition.TestReportTestData;
+import testReportComposition.TestStepReportStructure;
+
+public class SOI_3593_TC56_DS_D03_OfferGeneration_Temporary_ISDN {
+	String testName = "SOI_3593_TC56_DS_D03_OfferGeneration_Temporary_ISDN";
+
+	String initialTestDate=ExecStructure.formattedDate("dd-MM-yyyy HH:mm:ss");
+
+	long startTime=System.nanoTime();
+
+	List<TestStepReportStructure> testExecStructure = new ArrayList<TestStepReportStructure>();
+
+	List<TestReportTestData> testData = new ArrayList<TestReportTestData>();
+
+	List<TestLog> logStream = new ArrayList<TestLog>();
+
+	WebDriver driver;
+	
+
+	@BeforeClass
+	public void beforeClass() 
+	{
+		driver = ExecDriverClass.setupDriver();
+		
+		TestStructure.startTest(logStream,testName);
+		
+	}
+
+	
+	@Test
+	public void SOI_3593_TC56_DS_D03_OfferGeneration_Temporary_ISDN() throws Exception
+	{
+		String productD03="PABX";
+		
+		//String finalTestName = testName+"_"+productD03;
+		
+		try
+		{
+			SOI_3593.SOI_3593_TC01_DS_D03_OfferGeneration_General(testExecStructure, logStream, driver, testName, "Temporary ISDN");
+			SOI_3593.SOI_3593_TC01_PC_D03_OfferGeneration_General(testExecStructure, logStream, driver, testName, "Temporary ISDN");
+
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			TestLogger.logError(logStream, testName, "Test Execution Failed", e.toString());
+			throw new Exception (testName+" - Failed: ",e);
+		}
+	}
+
+		@AfterClass
+	public void afterClass() 
+	{
+		TestStructure.finishTest(testName, initialTestDate, startTime, testExecStructure, testData, driver, logStream);
+	}
+}
