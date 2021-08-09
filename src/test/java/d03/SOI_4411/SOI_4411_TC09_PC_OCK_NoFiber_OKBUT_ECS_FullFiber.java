a8bf41c412da279bc50d5bd918fe3255204d046d
ana.mota <ana.mota@innowave.tech>
2021-08-09 19:28:22 +0100

    Scripts finished from US SOI-4411

---
+++ src/test/java/d03/SOI_4411/SOI_4411_TC09_PC_OCK_NoFiber_OKBUT_ECS_FullFiber.java
@@ -0,0 +1,67 @@
+package d03.SOI_4411;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import delivery03.SOI_4100;
+import delivery03.SOI_4411;
+import executionDriver.ExecDriverClass;
+import executionTools.ExecStructure;
+import executionTools.TestStructure;
+import testLogBuilder.TestLog;
+import testLogger.TestLogger;
+import testReportComposition.TestReportTestData;
+import testReportComposition.TestStepReportStructure;
+
+public class SOI_4411_TC09_PC_OCK_NoFiber_OKBUT_ECS_FullFiber {
+	String testName = "SOI_4411_TC09_PC_OCK_NoFiber_OKBUT_ECS_FullFiber";
+
+	String initialTestDate=ExecStructure.formattedDate("dd-MM-yyyy HH:mm:ss");
+
+	long startTime=System.nanoTime();
+
+	List<TestStepReportStructure> testExecStructure = new ArrayList<TestStepReportStructure>();
+
+	List<TestReportTestData> testData = new ArrayList<TestReportTestData>();
+
+	List<TestLog> logStream = new ArrayList<TestLog>();
+
+	WebDriver driver;
+	
+
+	@BeforeClass
+	public void beforeClass() 
+	{
+		driver = ExecDriverClass.setupDriver();
+		
+		TestStructure.startTest(logStream,testName);
+		
+	}
+
+	
+	@Test
+	public void SOI_4411_TC09_PC_OCK_NoFiber_OKBUT_ECS_FullFiber() throws Exception
+	{
+		try
+		{
+			SOI_4411.SOI_4411_TC09_PC_OCK_NoFiber_OKBUT_ECS_FullFiber(testExecStructure, logStream, driver, testName);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			TestLogger.logError(logStream, testName, "Test Execution Failed", e.toString());
+			throw new Exception (testName+" - Failed: ",e);
+		}
+	}
+
+		@AfterClass
+	public void afterClass() 
+	{
+		TestStructure.finishTest(testName, initialTestDate, startTime, testExecStructure, testData, driver, logStream);
+	}
+}
